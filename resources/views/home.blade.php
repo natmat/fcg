004eebf3094dcbd97b378ed93cd32e5c39483868
Nathan Mattick <nathan.mattick@petards.com>
2020-12-31 21:23:25 +0000

    Up to adding the homepage images... needs layout tweaking

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row">
+        <div class="col-3 w-5 p-5">
+            <img src="https://www.cambridgeconsultants.com/sites/default/files/sir-trevor-uploads/4%20-%20Ofcom%201300x720.jpg" class="rounded-circle" style="width:200px">
+        </div>
+        <div class=col-9">
+            <div><h1>freeCodeGram</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>123</strong> posts</div>
+                <div class="pr-5"><strong>4567</strong> followers</div>
+                <div class="pr-5"><strong>89012</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeGram.org</div>
+            <div class="w-75">Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi.</div>
+            <div><a href="#">http://www.bbc.com</a></div>
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+        <div class="row">
+            <div class="col-4">
+                <img src="https://images.unsplash.com/photo-1609377567545-2999c0d31315?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="h-50"></img>
+            </div>
+            <div class="col-4">
+                <img src="https://images.unsplash.com/photo-1609400816977-ed9aa6240433?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="h-50"></img>
+            </div>
+            <div class="col-4">
+                <img src="https://images.unsplash.com/photo-1609407201073-a1e6cc3474a7?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="h-50"></img>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
