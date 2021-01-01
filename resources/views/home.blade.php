f92cf076b62347b2429aee6935c42edc527a0c00
Nathan Mattick <nathan.mattick@petards.com>
2021-01-01 09:26:30 +0000

    Added the diagrams, started registering users.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-3 w-5 p-5">
-            <img src="https://www.cambridgeconsultants.com/sites/default/files/sir-trevor-uploads/4%20-%20Ofcom%201300x720.jpg" class="rounded-circle" style="width:200px">
-        </div>
-        <div class=col-9">
-            <div><h1>freeCodeGram</h1></div>
-            <div class="d-flex">
-                <div class="pr-5"><strong>123</strong> posts</div>
-                <div class="pr-5"><strong>4567</strong> followers</div>
-                <div class="pr-5"><strong>89012</strong> following</div>
-            </div>
-            <div class="pt-4 font-weight-bold">freeCodeGram.org</div>
-            <div class="w-75">Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi.</div>
-            <div><a href="#">http://www.bbc.com</a></div>
-        </div>
-
+    <div class="container">
         <div class="row">
-            <div class="col-4">
-                <img src="https://images.unsplash.com/photo-1609377567545-2999c0d31315?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="h-50"></img>
+            <div class="col-3 p-5">
+                <img
+                    src="https://www.cambridgeconsultants.com/sites/default/files/sir-trevor-uploads/4%20-%20Ofcom%201300x720.jpg"
+                    class="rounded-circle img-fluid">
             </div>
-            <div class="col-4">
-                <img src="https://images.unsplash.com/photo-1609400816977-ed9aa6240433?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="h-50"></img>
-            </div>
-            <div class="col-4">
-                <img src="https://images.unsplash.com/photo-1609407201073-a1e6cc3474a7?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="h-50"></img>
+            <div class="col-9">
+                <div><h1>freeCodeGram</h1></div>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>123</strong> posts</div>
+                    <div class="pr-5"><strong>4567</strong> followers</div>
+                    <div class="pr-5"><strong>89012</strong> following</div>
+                </div>
+                <div class="pt-4 font-weight-bold">freeCodeGram.org</div>
+                <div class="w-75">Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque
+                    laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi.
+                </div>
+                <div><a href="#">http://www.bbc.com</a></div>
             </div>
         </div>
 
+        <div class="row pt-4">
+            <div class="col-4">
+                <img
+                    src="https://images.unsplash.com/photo-1609377567545-2999c0d31315?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                    class="h-50"></img>
+            </div>
+            <div class="col-4">
+                <img
+                    src="https://images.unsplash.com/photo-1609400816977-ed9aa6240433?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                    class="h-50"></img>
+            </div>
+            <div class="col-4">
+                <img
+                    src="https://images.unsplash.com/photo-1609407201073-a1e6cc3474a7?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                    class="h-50"></img>
+            </div>
+        </div>
     </div>
-</div>
 @endsection
