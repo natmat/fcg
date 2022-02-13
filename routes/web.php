ce5ba2acce3f49406323642c10c09790a928cc30
Nathan Mattick <nmattick@gmail.com>
2022-02-13 09:49:43 +0000

    laravel new fcp - start again

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-//Route::get('/home', [App\Http\Controllers\ProfilesController::class, 'index'])->name('home');
-
-Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
